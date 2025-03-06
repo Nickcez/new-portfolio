f3c03ff7f3b69fc0d5af40c91f34dd646e33c2fa
Nicolas Césaire <n.cesaire@outlook.com>
2025-03-05 19:58:25 -0500

    home completed

--- my-real-portfolio/src/App.js
+++ my-real-portfolio/src/App.js
@@ -1,10 +1,17 @@
+import React from 'react';
 import './App.css';
 import Header from './components/header/Header.jsx';
+import Home from './components/home/Home.jsx';
+import About from './components/about/About.jsx';
 
 const App = () => {
   return (
     <>
     <Header />
+    <main className='main'>
+      <Home />
+      <About />
+    </main>
     </>
   );
 }
