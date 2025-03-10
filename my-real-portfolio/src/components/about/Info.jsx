32511861cce6f9bc61c8649bf1bee0ac4818bb02
Nicolas Césaire <n.cesaire@outlook.com>
2025-03-10 13:36:53 -0400

    about and skills done

---
+++ my-real-portfolio/src/components/about/Info.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Info = () => {
+    return (
+        <div className="about__info grid">
+            <div className="about__box">
+            <i class='bx bx-award'></i>
+                <h3 className="about__title">Experience</h3>
+                <span className="about__subtitle">2 Years since I graduated from the Concordia University Bootcamp</span>
+            </div>
+
+            {/* <div className="about__box">
+            <i class='bx bx-briefcase'></i>
+                <h3 className="about__title"></h3>
+                <span className="about__subtitle"></span>
+            </div> */}
+
+            {/* <div className="about__box">
+                <h3 className="about__title">Support</h3>
+                <span className="about__subtitle">Online 24/7</span>
+            </div> */}
+        </div>
+    )
+}
+
+export default Info
