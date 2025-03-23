13cc7f2564dfedf6a7b56ca1da99033ad85e9f4a
Nicolas Césaire <n.cesaire@outlook.com>
2025-03-23 17:11:07 -0400

    contact done

--- my-real-portfolio/src/App.js
+++ my-real-portfolio/src/App.js
@@ -4,6 +4,8 @@ import Header from './components/header/Header.jsx';
 import Home from './components/home/Home.jsx';
 import About from './components/about/About.jsx';
 import Skills from './components/skills/Skills.jsx';
+import Contact from './components/contact/Contact.jsx';
+import Footer from './components/footer/Footer.jsx';
 
 const App = () => {
   return (
@@ -13,6 +15,8 @@ const App = () => {
       <Home />
       <About />
       <Skills />
+      <Contact />
+      {/* <Footer /> */}
     </main>
     </>
   );
--- my-real-portfolio/src/components/home/Social.jsx
+++ my-real-portfolio/src/components/home/Social.jsx
@@ -3,9 +3,9 @@ import React from "react";
 const Social = () => {
     return (
         <div className="home__social">
-            <a href="https://www.instagram.com/nickcez/" className="home__social-icon" target="_blank">
+            {/* <a href="https://www.instagram.com/nickcez/" className="home__social-icon" target="_blank">
                 <i class="uil uil-instagram"></i>
-            </a>
+            </a> */}
             <a href="https://github.com/Nickcez" className="home__social-icon" target="_blank">
                 <i class="uil uil-github-alt"></i>
             </a>
