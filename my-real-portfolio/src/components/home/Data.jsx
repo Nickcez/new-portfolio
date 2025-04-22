2da0b7164c22a1724d4776cad85630bbe7251bc6
“Nicolas <n.cesaire@outlook.com>
2025-04-21 22:32:42 -0400

    Beatifullll

--- my-real-portfolio/src/components/home/Data.jsx
+++ my-real-portfolio/src/components/home/Data.jsx
@@ -55,7 +55,7 @@ const Data = () => {
                 </svg> */}
             </h1>
             <h3 className="home__subtitle">Full Stack Developer</h3>
-            <p className="home__description">Web developer from Montreal. Hard work and always finding a way to win.</p>
+            <p className="home__description">Web developer from Montreal.</p>
             <a href="#contact" className="button button--flex">
                 Say Hello
                 <svg
